c1f0d0c5201ab218e4957acb01f3ffb546fbe1f6
kaisujay <kai.sujay@gmail.com>
2024-09-08 16:08:03 +0530

    Added basic "CustomException"

---
+++ GameStoreBackEndV1/ServiceLogic/ExceptionService/CustomException.cs
@@ -0,0 +1,42 @@
+﻿namespace GameStoreBackEndV1.ServiceLogic.ExceptionService
+{
+    public class NotFoundException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public NotFoundException() : base()
+        {
+            StatusCode = StatusCodes.Status404NotFound;
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+            StatusCode = StatusCodes.Status404NotFound;
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+            StatusCode = StatusCodes.Status404NotFound;
+        }
+    }
+
+    public class ExternalResourceNotFoundException : Exception
+    {
+        public int StatusCode { get; set; }
+
+        public ExternalResourceNotFoundException() : base()
+        {
+            StatusCode = StatusCodes.Status400BadRequest;
+        }
+
+        public ExternalResourceNotFoundException(string message) : base(message)
+        {
+            StatusCode = StatusCodes.Status400BadRequest;
+        }
+
+        public ExternalResourceNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+            StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+}
