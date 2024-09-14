6f30b549aba87f882f54ce1accfbbb0c69abdc3f
kaisujay <kai.sujay@gmail.com>
2024-09-14 16:55:45 +0530

    SMALL: Updated "CreateRoleDto"

--- GameStoreBackEndV1/ObjectLogic/ObjectDTOs/Role/CreateRoleDto.cs
+++ GameStoreBackEndV1/ObjectLogic/ObjectDTOs/Role/CreateRoleDto.cs
@@ -4,10 +4,6 @@ namespace GameStoreBackEndV1.ObjectLogic.TableDataModels
 {
     public class CreateRoleDto
     {
-        [Required(ErrorMessage = "RoleId is required")]
-        public Guid RoleId { get; set; }
-
-
         [Required(ErrorMessage = "Role Name is required")]
         public string RoleName { get; set; }
     }
