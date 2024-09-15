73ccd765e2eb96493c5405f1659928de277d7e25
kaisujay <kai.sujay@gmail.com>
2024-09-15 14:21:41 +0530

    SMALL: Updated Dto "CreatePlatformTypeDto"

--- GameStoreBackEndV1/ObjectLogic/ObjectDTOs/PlayformType/CreatePlatformTypeDto.cs
+++ GameStoreBackEndV1/ObjectLogic/ObjectDTOs/PlayformType/CreatePlatformTypeDto.cs
@@ -4,10 +4,6 @@ namespace GameStoreBackEndV1.ObjectLogic.TableDataModels
 {
     public class CreatePlatformTypeDto
     {
-        [Required(ErrorMessage = "PlatformTypeId is required")]
-        public Guid PlatformTypeId { get; set; }
-
-
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
     }
